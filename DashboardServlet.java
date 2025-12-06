c468338f792a988dd8b1b790f879f590dddd6f45
Allan Zhang <145911057+Allan546@users.noreply.github.com>
2025-12-06 00:34:05 -0500

    new

---
+++ DashboardServlet.java
@@ -0,0 +1,21 @@
+package edu.cs.budgetbuddy.servlet;
+
+import edu.cs.budgetbuddy.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/dashboard")
+public class DashboardServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+}
